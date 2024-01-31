9fd4ca166b77c99a33e2342dc5439d03e4f71db5
daniellizarazoo <daniellizarazo015@gmail.com>
2024-01-31 16:31:13 -0500

    new_one

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from fastapi.templating import Jinja2Templates 
-from fastapi import FastAPI,Request
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+import uvicorn
+
 templates = Jinja2Templates(directory="templates")
 
 app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
-async def main_html(request: Request,fname:str|None=None,city:str|None=None,
-                    eaddress:str|None=None,phone:str|None=None):
-    
+async def main_html(
+    request: Request, 
+    fname: str | None = None, 
+    city: str | None = None,
+    eaddress: str | None = None,
+    phone: str | None = None
+):
     return templates.TemplateResponse("/index.html", {"request": request})
+
+if __name__ == "__main__":
+    # Run the app using uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=80)
